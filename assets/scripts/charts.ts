6940dad8bf36c01bb7f718e8dfc245cbafbe9985
DESKTOP-OT9KKKV\ISTON <214447325@qq.com>
2022-11-03 10:48:56 +0800

    babylon添加图表移动

--- assets/scripts/charts.ts
+++ assets/scripts/charts.ts
@@ -15,49 +15,19 @@ export function chartsInit(scriptMap, scene) {
         for(let i of data.charts) {
             let style = i.style;
             $('#test').append(`<div id="${i.id}" ><div>`)
-            // https://cdn.bootcss.com/echarts/4.2.1/echarts.simple.min.js
-            // $('body').append(` <script src="https://cdn.bootcss.com/echarts/4.2.1/echarts.simple.min.js" type="text/javascript"></script>`)
             let dom = document.getElementById(i.id);
             // @ts-ignore
             dom?.style = style
             console.log('333333333333333')
-            // console.log(require('echarts'))
-            // setChart(i.id,i.option);
             let echarts = require('echarts');
+            if(echarts) {
+                let chartDom: any = document.getElementById(i.id);
+                // @ts-ignore
+                let myChart = echarts.init(chartDom);
+                (i.option) && myChart.setOption(i.option);
+            }
             
-            let chartDom: any = document.getElementById(i.id);
-            // @ts-ignore
-            let myChart = echarts.init(chartDom);
-            (i.option) && myChart.setOption(i.option);
         }
     }
-    
-    // let charts = scene.charts ?? []
-    // console.log(charts);
-    // let $ = require('jquery');
-    // let chartArray = scene.metadata.postProcesses
-    // let test = chartArray ? chartArray.test : ''
-    // console.log(chartArray);
-    // console.log(test);
-    // if(chartArray && chartArray.length > 0) {
-        // for(let i of chartArray) {
-        //     let style = i.style;
-        //     $('#test').append(`<div id="${i.id}" style="${style}"><div>`)
-        //     setChart(i.id,i.option);
-        // }
-    // }
-
-
-    // console.log()
 
 }
-
-// export function setChart(id,option) {
-//     console.log('*******************************');
-//     console.log(id)
-//     console.log(option)
-    // let echarts = require('echarts');
-    // let chartDom: any = document.getElementById(id);
-    // let myChart = echarts.init(chartDom);
-    // option && myChart.setOption(option);
-// }
